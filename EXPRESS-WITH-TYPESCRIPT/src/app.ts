5e2d34058025f11abd96d159523b75efe6498c39
seoyoungsoo <iulove37@naver.com>
2021-08-07 20:56:22 +0900

    Refactor: 싱글톤 패턴 적용

--- EXPRESS-WITH-TYPESCRIPT/src/app.ts
+++ EXPRESS-WITH-TYPESCRIPT/src/app.ts
@@ -1,30 +1,52 @@
 import * as express from 'express';
 import catsRouter from './routes/cats';
 
-const app: express.Express = express();
 const port: number = 8000;
 
-// logging middleware
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('using logging Middleware');
-    next();
-  },
-);
+// 싱글톤 패턴을 적용
+class Server {
+  public app: express.Application;
 
-// JSON middleware
-app.use(express.json());
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-app.use(catsRouter);
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-// 404 middleware
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('using error Middleware');
-    res.send({ error: '404 not found error' });
-  },
-);
+  private setMiddleware() {
+    // logging middleware
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      console.log('using logging Middleware');
+      next();
+    });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    // JSON middleware
+    // POST로 들어온 JSON 형식을 읽어들일 수 있게함
+    this.app.use(express.json());
+
+    this.setRoute();
+
+    // 404 middleware
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      console.log('using error Middleware');
+      res.send({ error: '404 not found error' });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  }
+}
+
+const init = () => {
+  const server = new Server();
+  server.listen();
+};
+
+init();
